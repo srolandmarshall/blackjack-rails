62e213a903123df6eb5bebf97378fb0c5188ab30
S. Roland Marshall <samuel.r.marshall@gmail.com>
2021-07-25 21:22:44 -0400

    added pile logic

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,5 +1,5 @@
 class Deck < ApplicationRecord
-  def self.create_with_api(decks, title = 'New Deck')
+  def self.create_with_api(decks = 1, title = 'New Deck')
     @data = HTTParty.get("http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=#{decks}")
     deck_id = @data['deck_id']
     remaining_cards = @data['remaining']
@@ -13,11 +13,10 @@ class Deck < ApplicationRecord
   end
 
   def shuffle!
-    url = "http://deckofcardsapi.com/api/deck/#{deck_id}/shuffle/"
-    puts url
     puts "Shuffling deck #{deck_id}"
-
+    url = "http://deckofcardsapi.com/api/deck/#{deck_id}/shuffle/"
     response = HTTParty.get(url)
+
     self.remaining_cards = response['remaining']
     self.last_card = {}
     save
@@ -31,9 +30,35 @@ class Deck < ApplicationRecord
 
   def draw(num_cards)
     response = HTTParty.get("http://deckofcardsapi.com/api/deck/#{deck_id}/draw/?count=#{num_cards}")
-    puts response.inspect
     self.remaining_cards = response['remaining']
     self.last_card = response['cards'].last
     save
+    response['cards']
+  end
+
+  # https://deckofcardsapi.com/api/deck/<<deck_id>>/pile/<<pile_name>>/add/?cards=AS,2S
+  def add_to_pile(cards_hash, pile_name)
+    cards = get_card_codes_string(cards_hash)
+    url = "http://deckofcardsapi.com/api/deck/#{deck_id}/pile/#{pile_name}/add/?cards=#{cards}"
+    response = HTTParty.get(url)
+  end
+
+  def cards_in_pile(pile_name)
+    # https://deckofcardsapi.com/api/deck/<<deck_id>>/pile/<<pile_name>>/list/
+    url = "http://deckofcardsapi.com/api/deck/#{deck_id}/pile/#{pile_name}/list/"
+    response = HTTParty.get(url)
+  end
+
+  def draw_and_add_to_pile(num_cards, pile_name)
+    cards = get_card_codes(draw(num_cards))
+    add_to_pile(cards, pile_name)
+  end
+
+  def get_card_codes(cards)
+    cards.map { |c| c['code'] }
+  end
+
+  def get_card_codes_string(cards)
+    cards.map { |c| c['code'] }.join(',')
   end
 end
